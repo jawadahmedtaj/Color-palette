5f1186ab5806d27ba5deb81ddc2ee383d7b50a04
Jawad Ahmed <jawadahmedtaj@gmail.com>
2020-03-31 22:18:58 +0200

    footer added

--- src/Palette.js
+++ src/Palette.js
@@ -26,10 +26,10 @@ export default class Palette extends Component {
   }
 
   render() {
-    const { colors } = this.props.palette;
+    const { colors, paletteName, emoji } = this.props.palette;
     const { level, format } = this.state;
     const colorBoxes = colors[level].map(color => (
-      <ColorBox background={color[format]} name={color.name} />
+      <ColorBox background={color[format]} name={color.name} key={color.id} />
     ));
     return (
       <div className="Palette">
@@ -38,12 +38,16 @@ export default class Palette extends Component {
           changeLevel={this.changeLevel}
           handleChange={this.changeFormat}
         />
-        {/* navbar goes here */}
+
         <div className="Palette-colors">
           {/* color boxes go here */}
           {colorBoxes}
         </div>
-        {/* footer goes here */}
+
+        <footer className="Palette-footer">
+          {paletteName}
+          <span className="emoji">{emoji}</span>
+        </footer>
       </div>
     );
   }
